2aabe230bbb1bb454cc16c273e31f0b43cc05ac6
minh.phamquang <phamquangminhex@gmail.com>
2019-07-17 23:47:23 +0700

    - [UI] Add game title at main menu

--- src/game/GS_MainMenu.js
+++ src/game/GS_MainMenu.js
@@ -9,6 +9,11 @@ class GS_MainMenu extends PIXI.Container {
 
         this.texture_Bg = PIXI.Texture.from('data/image/background.png');
         this.bg = null;
+
+        this.texture_Title = PIXI.Texture.from('data/image/title.png');
+        this.title = null;
+        this.hueCounter;
+
         APP.addChild(this);
     }
     
@@ -30,7 +35,17 @@ class GS_MainMenu extends PIXI.Container {
         this.bg.x = GameConfig.width / 2;
         this.bg.y = GameConfig.height/ 2;
         this.bg.anchor.set(0.5);
+        this.bg.tint = 0x555555;
         this.addChildAt(this.bg, 0);
+        
+        this.title = new PIXI.Sprite(this.texture_Title);  
+        this.title.anchor.set(0.5);
+        this.title.x = this.play_Button.x;
+        this.title.y =  this.play_Button.y - this.title.height - this.play_Button.height / 2;
+        this.addChild(this.title);
+
+        let colorMatrix = new PIXI.filters.ColorMatrixFilter();
+        this.title.filters = [colorMatrix];
 
         this.x = (APP.renderer.width - GameConfig.width) / 2;
     }
@@ -39,8 +54,14 @@ class GS_MainMenu extends PIXI.Container {
         const GS_Ingame = require('../game/GS_Ingame');
         StateManager.SwitchState(GS_Ingame);
     }
-
     Update(deltaTime){
+        this.hueCounter += deltaTime;
+
+        if (this.hueCounter > 360) {
+            this.hueCounter = 0;
+        }
+
+        this.title.filters[0].hue(this.hueCounter);
     }
 
     Unload() {
